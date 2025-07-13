388645306ef942f63763edd6d47ea6594d6ca4ad
Nafis Fairuza <nafifairuza@gmail.com>
2025-07-13 20:06:56 +0700

    Create clean_and_eda.py

---
+++ python_cleaning_eda/clean_and_eda.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+#Load dataset
+df = pd.read_csv(r"D:\dataset\student_habits_performance\student_habits_performance.csv")
+print(df)
+df.info()
+
+# Data cleaning and check missing values
+df["parental_education_level"].fillna("Unknown",inplace=True)
+print(df.isnull().sum())
+df.drop("student_id", axis=1, inplace=True)
+print(df.head())
+
+# Visualization (Study hours impact to exam scores)
+plt.figure(figsize=(8, 6))
+sns.scatterplot(data=df, x="exam_score", y="study_hours_per_day", color="Olivedrab")
+plt.title("The Impact Study Hours on Exam Scores")
+plt.xlabel("exam score")
+plt.ylabel("study hours per day")
+plt.tight_layout()
+plt.savefig("plot_scatter_exam_vs_studyhours.png")
+plt.show()
+
+
+df.to_csv(r"D:\dataset\student_habits_performance\student_habits_performance_EDA.csv", index=False)
